21bbe15421b1aeaf91ae6b233c014b8da6ca753b
ugpec79 <ujjawalguptapec@gmail.com>
2023-09-09 17:28:28 +0530

    Auth Changes

---
+++ src/constants/common.js
@@ -0,0 +1,9 @@
+export class Common {
+    static primaryColor = "#0075FF";
+    static AUTHORIZATION = "Authorization";
+    static BEARER = "Bearer";
+    static USERNAME = "Username";
+    static PHOTO = "photoUrl";
+    static EMAIL = "Email";
+    static DESIGNATION = "Designation";
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,39 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
-library.add(faEye, faEyeSlash)
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useLocalStorage } from "@uidotdev/usehooks";
+import { Common } from '@/constants/common';
+import { parseCookies, setCookie } from 'nookies'
 import { Login } from '../components/Login'
 import Landing from '@/components/Landing'
 
 
 
 
-export default function Home() {
-  const[isAuthenticated, setAuthenticated]=useState(false);
-  
-  useEffect(()=>{
-    
-    const value=localStorage.getItem("isAuthenticated")||false;
-    setAuthenticated(value);
-    
- 
-  },[])
+export default function Home(props) {
+ // props can be passed on to the landing page component
   return (
     <div>
-    {isAuthenticated?<Landing/>:<Login/>}
+    {props.token?(<Landing/>):(<Login/>)}
       
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const { req } = context;
+  const cookies = parseCookies({ req });
+  const token = cookies[Common.AUTHORIZATION];
+
+  if (token) {
+    const username = cookies[Common.USERNAME];
+    const photo = cookies[Common.PHOTO];
+      return {
+         props:{token,username,photo}
+      };
+  }
+
+  return {
+      props: {},
+  };
 }
