af36a27e86b4452918e01c1b80d929a47170ad73
haraseessingh <harasees1202@gmail.com>
2023-12-30 13:39:11 +0530

    added searchbar with user search

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,29 +1,31 @@
 import DialogPopup from "@/components/common/DialogPopup";
 import getServerCookieData from "@/lib/getServerCookieData";
 import logout from "@/lib/logout";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import { useState } from "react";
+import {useSession} from "next-auth/react";
+import {useRouter} from "next/router";
+import {useState} from "react";
+import SearchBarWithDropdown from "@/components/SearchBarWithDropdown";
 
 function AdminDashboard() {
     const [showLogoutDialog, setShowLogoutDialog] = useState(false);
     const router = useRouter();
+    const [isUserSearch, setIsUserSearch] = useState(true)
 
-    const { data: session } = useSession();
+    const {data: session} = useSession();
 
     return (
         <div>
-            AdminDashboard{" "}
-            {showLogoutDialog ? (
-                <DialogPopup
+            <div class='flex items-center justify-between p-2'>
+                AdminDashboard{" "}
+                {showLogoutDialog && <DialogPopup
                     errorDescription="Are you sure you want to logout?"
                     errorTitle="Logout"
                     handleAction={() => {
                         logout(router, session);
                     }}
                     handleClose={() => setShowLogoutDialog(false)}
-                />
-            ) : (
+                />}
+
                 <button
                     className="acm-button"
                     aria-label="Logout"
@@ -31,7 +33,20 @@ function AdminDashboard() {
                 >
                     Logout
                 </button>
-            )}
+            </div>
+
+
+            <div class='h-screen bg-blue-300'>
+                <div class='flex justify-center bg-amber-200'>
+                    <SearchBarWithDropdown isUserSearch={isUserSearch}/>
+                    <button onClick={() => setIsUserSearch((prevState) => !prevState)}
+                            className='mb-auto p-2 bg-blue-900 border-2 border-blue-900 ml-2 rounded-md text-white min-w-[10vw]'>
+                        {
+                            `Toggle ${isUserSearch ? 'Events' : 'Users'}`
+                        }
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
@@ -39,7 +54,7 @@ function AdminDashboard() {
 export default AdminDashboard;
 
 export async function getServerSideProps(context) {
-    const { data } = getServerCookieData(context);
+    const {data} = getServerCookieData(context);
 
     const token = data?.token;
 
